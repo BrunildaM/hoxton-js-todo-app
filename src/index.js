5be86e56c2224da22af888221db47b81d135e03f
Brunilda Muco <brunildamuco6@gmail.com>
2022-06-27 09:12:30 +0200

    completed todo-s

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let state = {
         { id:1, title: 'Go Shopping', completed:false },
         { id:2, title: 'Work Out', completed:false },
         { id:3, title: 'Go to cinema', completed:false },
-        { id:4, title: 'See the doctor', completed:false },
+        { id:4, title: 'See the doctor', completed:true },
         { id:5, title: 'Learn Javascript', completed:false },
         { id:6, title: 'Finish Assigments', completed:true },
     ],
@@ -59,32 +59,93 @@ function doNotShowCompleted(){
 }
 
 
+function renderCreateTodoForm() {
+    let formEl = document.createElement('form')
+    let titleInput = document.createElement('input')
+    titleInput.type = 'text'
+    let createTodoButton = document.createElement('button')
+    createTodoButton.textContent = 'Create TODO'
+
+    formEl.addEventListener('submit', function (event) {
+      event.preventDefault()
+      createTodo(titleInput.value)
+      render()
+    })
+  
+    formEl.append(titleInput, createTodoButton)
+    document.body.append(formEl)
+  }
+
+
+  function renderShowCompleted() {
+    let showCompletedLabel = document.createElement('label')
+    showCompletedLabel.textContent = 'Show completed: '
+  
+    let showCompletedCheckbox = document.createElement('input')
+    showCompletedCheckbox.type = 'checkbox'
+    if (state.showCompleted) showCompletedCheckbox.checked = true
+    showCompletedCheckbox.addEventListener('click', function () {
+        doNotShowCompleted()
+      render()
+    })
+  
+    showCompletedLabel.append(showCompletedCheckbox)
+    document.body.append(showCompletedLabel)
+  }
+
+
+
+function createH2Title(title){
+    let h2Ele = document.createElement('h2');
+    h2Ele.className = 'header'
+    h2Ele.textContent = title
+
+    return h2Ele
+}
+
 function render(){
 
     document.body.textContent = '';
 
-    let todoListUncompleted = document.createElement('ul');
-    let todoListCompleted = document.createElement('ul');
+    let optionsTitle = createH2Title('Options')
+    document.body.append(optionsTitle);
 
+    renderShowCompleted()
+    renderCreateTodoForm()
+    let todoList = document.createElement('ul');
+    todoList.className = 'todo-list'
+    
+    let completedTodo = document.createElement('ul');
+    completedTodo.className = 'completed-list'
+   
     for (let todo of state.todos){
-
+        
         let liElement = document.createElement('li');
+        liElement.textContent = todo.title 
 
-       
-        liElement.textContent = todo.title
+        liElement.addEventListener('click', function(){
+            toggleTodo(todo.title)
+            render()
+        })
         
         if (todo.completed){
             liElement.className = 'todo completed'
-            todoListCompleted.append(liElement)
+            completedTodo.append(liElement)
         } else {
-            todoListUncompleted.append(liElement)
+            liElement.className = 'todo'
+            todoList.append(liElement)
         }
-
-
     }
 
-    document.body.append(todoListUncompleted, todoListCompleted);
+    // H2 Title for todos
+    let todoTitle = createH2Title('Todo')
+    document.body.append(todoTitle);
 
+    document.body.append(todoList);
+    // H2 Title
+    let completedTitle = createH2Title('Completed')
+    document.body.append(completedTitle);
+    document.body.append(completedTodo);
 }
 
-render()
+render();
